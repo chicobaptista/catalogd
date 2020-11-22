cda584d2bf2f9fb62146355b1cae62e0d895f58b
Francisco Llamosas Baptista <f.baptista@ecotrace.info>
2020-11-22 14:15:48 -0300

    [Feature] Implementa input para add item

--- catalogd-app/src/app/home/home.page.ts
+++ catalogd-app/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +8,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
   // itemList: Item[] = []
   itemList: Item[] = [{titulo: "Aaaahhhh", subtitulo: "Lorem ipusum quae dolores sunt"}, {titulo: "Beeeeeh", subtitulo: "Lorem ipusum quae dolores sunt"}]
 
-  addItem() {
+  addItem(titulo: String) {
     this.itemList.push(
-      {titulo: "Ceeeehhhh", subtitulo: "Lorem ipusum quae dolores sunt"}
+      {titulo: titulo, subtitulo: "Lorem ipusum quae dolores sunt"}
     )
+  }
+
+  async addItemPrompt(){
+    const alert = await this.alertController.create(
+      {
+        header: "Novo item",
+        inputs: [
+          {
+            name: "titulo",
+            type: "text",
+            placeholder: "Nome do item"
+          }
+        ],
+        buttons: [
+          {
+            text: "Cancelar",
+            role: "cancel"
+          },
+          {
+            text: "Confirmar",
+            handler: (data) => {
+              this.addItem(data.titulo)
+            }
+          }
+        ]
+      })
+    await alert.present()
   }
 }
 
