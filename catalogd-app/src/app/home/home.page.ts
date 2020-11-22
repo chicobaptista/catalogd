fc6ec71e30c6d68b153b0a5b954172a8d7dbed04
Francisco Llamosas Baptista <f.baptista@ecotrace.info>
2020-11-22 14:00:57 -0300

    [Feature] Implementa naive add fab button

--- catalogd-app/src/app/home/home.page.ts
+++ catalogd-app/src/app/home/home.page.ts
@@ -12,6 +12,11 @@ export class HomePage {
   // itemList: Item[] = []
   itemList: Item[] = [{titulo: "Aaaahhhh", subtitulo: "Lorem ipusum quae dolores sunt"}, {titulo: "Beeeeeh", subtitulo: "Lorem ipusum quae dolores sunt"}]
 
+  addItem() {
+    this.itemList.push(
+      {titulo: "Ceeeehhhh", subtitulo: "Lorem ipusum quae dolores sunt"}
+    )
+  }
 }
 
 
